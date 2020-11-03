da4c735f31be9e9c5847b62666cb7789952452ef
kaiomagalhaes <me@kaiomagalhaes.com>
2020-11-03 18:58:12 -0300

    install agnostic design lib

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Row, Container, Col } from '@codelittinc/agnostic-design-system';
+import '@codelittinc/agnostic-design-system/dist/main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Container>
+      <Row>
+        <Col sm={5}>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button onClick={() => alert('try again')} label={"potato"} />
+            </a>
+          </header>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
